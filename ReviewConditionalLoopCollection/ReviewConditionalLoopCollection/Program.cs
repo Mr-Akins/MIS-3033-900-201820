1c2d66ea2847bacbb1f8f43f78c4e000861b80e2
Acke9387 <Adam@ou.edu>
2019-01-17 17:31:27 -0600

    Initial Project creation

---
+++ ReviewConditionalLoopCollection/ReviewConditionalLoopCollection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewConditionalLoopCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
