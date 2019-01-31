1fa62a47406c9c6c2f73c0b5138b717c70eb16fb
Acke9387 <Adam@ou.edu>
2019-01-31 17:25:46 -0600

    Added classes review project

---
+++ ReviewConditionalLoopCollection/ReviewClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student adam = new Student();
+            adam.FirstName = "Adam";
+            adam.LastName = "Ackerman";
+            adam.GPA = 1.5;
+            adam.IsOnProbation = true;
+            adam.SoonerID = 1;
+
+            Student tim = new Student(2, "Timothy", "Fisher", 85000);
+
+            Console.ReadKey();
+        }
+    }
+}
